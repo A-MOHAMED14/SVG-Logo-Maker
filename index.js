2bede5a714820210fbc856cfc94ade03c59b4d91
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-07-01 11:36:05 +0100

    Use inquirer to ask user a series of questions

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 const inquirer = require("inquirer");
 
+inquirer.prompt([
+  {
+    type: "input",
+    name: "text",
+    message: "Enter upto 3 characters",
+  },
 
+  {
+    type: "input",
+    name: "textColour",
+    message: "Enter a text colour",
+  },
+
+  {
+    type: "list",
+    name: "shape",
+    message: "Choose a shape:",
+    choices: ["circle", "triangle", "square"],
+  },
+
+  {
+    type: "input",
+    name: "shapeColour",
+    message: "Enter a shape colour",
+  },
+]);
