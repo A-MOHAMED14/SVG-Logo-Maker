5f2c8ee0ceb50e3f972d9a9f94780dbae74555a7
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-07-01 11:38:32 +0100

    Store users feedback and catch any errors

--- index.js
+++ index.js
@@ -1,28 +1,37 @@
 const inquirer = require("inquirer");
 
-inquirer.prompt([
-  {
-    type: "input",
-    name: "text",
-    message: "Enter upto 3 characters",
-  },
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "text",
+      message: "Enter upto 3 characters",
+    },
 
-  {
-    type: "input",
-    name: "textColour",
-    message: "Enter a text colour",
-  },
+    {
+      type: "input",
+      name: "textColour",
+      message: "Enter a text colour",
+    },
 
-  {
-    type: "list",
-    name: "shape",
-    message: "Choose a shape:",
-    choices: ["circle", "triangle", "square"],
-  },
+    {
+      type: "list",
+      name: "shape",
+      message: "Choose a shape:",
+      choices: ["circle", "triangle", "square"],
+    },
 
-  {
-    type: "input",
-    name: "shapeColour",
-    message: "Enter a shape colour",
-  },
-]);
+    {
+      type: "input",
+      name: "shapeColour",
+      message: "Enter a shape colour",
+    },
+  ])
+  .then((answers) => {
+    console.log("ANSWERS:", answers);
+  })
+  .catch((error) => {
+    error.isTtyError
+      ? console.error("Couldn't be rendered in the current environment")
+      : console.error("Something else went wrong...", error);
+  });
